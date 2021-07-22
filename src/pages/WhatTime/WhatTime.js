1d943688d8f08fda67f69212708811f7a82a95bd
Antonin <akeuck@snapp.fr>
2021-07-22 17:55:18 +0200

    #11 [ADD] Form and clean clean

--- src/pages/WhatTime/WhatTime.js
+++ src/pages/WhatTime/WhatTime.js
@@ -1,88 +1,103 @@
 import React, {useState, useEffect} from 'react'
 import moment from 'moment';
 import timezone from 'moment-timezone'
-import {localesLang} from '../../config';
+import {localesLang, whisprUser} from '../../config';
 import {MONTH, DAY} from '../../utils/dateFunctions'
-import {whisprUser} from '../../config';
 
+export const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
 
-const WhatTimeIsIt = ({chosenTimeZone, timeZoneSearch, name, city}) => {
+    return {
+        value,
+        setValue,
+        reset: () => setValue(''),
+        bind: {
+            value,
+            onChange: event => {
+                setValue(event.target.value);
+            }
+        }
+    }
+}
+
+function useGetTimeIn(date, chosenTimeZone, timeZoneSearch) {
     const [franceMoment, setFranceMoment] = useState(moment().tz(timeZoneSearch));
-    const [localMoment, setLocalMoment] = useState(franceMoment.tz(timeZoneSearch));
+    // const [localMoment, setLocalMoment] = useState(franceMoment.tz(timeZoneSearch));
     const [strLocalDay, setStrLocalDay] = useState("");
     const [strLocalHour, setStrLocalHour] = useState("");
-
-    const [counter, setCounter] = React.useState(60);
-
-    // Third Attempts
-    // React.useEffect(() => {
-    //     setTimeout(() => {
-    //         setCounter(counter - 1)
-    //     }, 1000);
-    // }, [counter]);
+    const [delay, setDelay] = useState(0);
 
     useEffect(() => {
-        // const franceMoment = moment().tz(chosenTimeZone);
-        // const localMoment = franceMoment.tz(timeZoneSearch);
-        // const newDate = franceMoment.tz(timeZoneSearch).format("YYYY-MM-DD HH:mm:ss");
-        // const localDate = new Date(newDate);//.toLocaleString(localesLang, {weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "numeric"});
-
+        setDelay(100);
         setTimeout(() => {
-            setFranceMoment(moment().tz(chosenTimeZone));
-            setLocalMoment(franceMoment.tz(timeZoneSearch));
+            setFranceMoment(moment(date).tz(chosenTimeZone));
+            // setLocalMoment(franceMoment.tz(timeZoneSearch));
             const newDate = franceMoment.tz(timeZoneSearch).format("YYYY-MM-DD HH:mm:ss");
             const localDate = new Date(newDate);
-            console.log('heure : ', Date.now());
             setStrLocalDay(localDate.toLocaleString(localesLang, {weekday: "long", month: "long", day: "numeric"}));
-            setStrLocalHour(localDate.toLocaleString(localesLang, {hour: "numeric", minute: 'numeric', second:'numeric'}));
-        }, 1000);
-        // return clearInterval(timer);
-    }, [chosenTimeZone, franceMoment, timeZoneSearch]);
+            setStrLocalHour(localDate.toLocaleString(localesLang, date ? {hour: "numeric", minute: 'numeric'} : {hour: "numeric", minute: 'numeric', second:'numeric'}));
+        }, delay);
+    }, [chosenTimeZone, date, delay, franceMoment, timeZoneSearch]);
+    return ({
+        strLocalDay: strLocalDay,
+        strLocalHour: strLocalHour,
+    })
+}
 
-    console.log('chosenTimeZone : ', chosenTimeZone);
-    console.log('timeZoneSearch : ', timeZoneSearch);
-    // const month = MONTH[localMoment.month() + 1];
-    // const day = DAY[localMoment.day()];
-    // console.log('hour : ', localMoment.hour());
-    // console.log('month : ', MONTH[localMoment.month() + 1])
-    // console.log('localesLang', localesLang);
-    // console.log('heure à :', timeZoneSearch, ":", localMoment.toLocaleString({weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "numeric"}));
-    // const newDate = franceMoment.tz(timeZoneSearch).format("YYYY-MM-DD HH:mm:ss");
-    // console.log('str : ', newDate);
-    // const localDate = new Date(newDate);//.toLocaleString(localesLang, {weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "numeric"});
-    // console.log('localDate :', localDate.toLocaleString());
-    // const strLocalDay = localDate.toLocaleString(localesLang, {weekday: "long", month: "long", day: "numeric"});
-    // const strLocalHour = localDate.toLocaleString(localesLang, {hour: "numeric", minute: 'numeric', second:'numeric'});
-    // console.log();
+const WhatTimeIsIt = ({date, chosenTimeZone, timeZoneSearch, name, city}) => {
+    const timeZone = useGetTimeIn(date, chosenTimeZone, timeZoneSearch);
+
+    useEffect(() => {
+
+    }, [timeZone]);
 
     return (
         <div>
             Pour {name} à {city} nous sommes :
             <p>
-            {strLocalDay} il est {strLocalHour}
+            {timeZone.strLocalDay} il est {timeZone.strLocalHour}
             </p>
         </div>
     );
 }
 
 export const WhatTime = () => {
-    const local = new Date();
+    const {value:formDate, bind:bindFormDate, reset:resetFormDate} = useInput('');
+    const {value:formTime, bind:bindFormTime, reset:resetFormTime} = useInput('');
+    const [wantedDate, setWantedDate] = useState();
 
     return (
         <div>
-            text
+            <form onSubmit={(event) => {
+                event.preventDefault();
+                const myNewDate = new Date(formDate);
+                myNewDate.setHours(formTime.split(':')[0] , formTime.split(':')[1]);
+                setWantedDate(myNewDate)
+                }}
+                onReset={() => {
+                    resetFormDate();
+                    resetFormTime();
+                    setWantedDate();
+                }}>
+                {/* <label> */}
+                    Quelle date ?
+                    <input id="inputsm" required className="form-control champs" name="event date" type="date" {...bindFormDate} />
+                    <input id="inputsm" required className="form-control champs" name="event date" type="time" {...bindFormTime} />
+                {/* </label> */}
+                    <input type="submit" />
+                    <input type="reset" />
+            </form>
             {whisprUser.map((item, index) => (
                 <WhatTimeIsIt
-                // key={index}
+                date={wantedDate}
                 chosenTimeZone={'Europe/Paris'}
                 timeZoneSearch={item.timeZone}
                 name={item.name}
                 city={item.city}
+                index={index}
+                key={index}
                 />
             ))}
-            {/* {utcDate} */}
-            {/* {local.getUTCSeconds()} */}
-            {/* <text style={{textAlign: 'center', width: '100%'}}>hello</text> */}
         </div>
     );
 };
